61f5433fdc57a4eb4d3cb601352114234fbe3c72
ManuelChivari97 <chivari_manuel2010@yahoo.com>
2022-03-14 19:48:40 +0200

    Added the HomePage structure

---
+++ src/test/java/test/homepageTest/homepageTest.java
@@ -0,0 +1,15 @@
+package test.homepageTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import test.BaseTest;
+
+public class homepageTest extends BaseTest {
+
+    @Test
+    public void testLogoIsDisplayed(){
+        Assert.assertTrue(homePage.checkLogo(), "Logo is not displayed");
+
+
+    }
+}
